345d6d5869e6bcb9aa7030bf0e6d73292e0e388e
skyminju9 <skyminju9@naver.com>
2024-10-15 15:14:58 +0900

    style: font settings modified

--- src/frontend/book-recommend-service/src/App.js
+++ src/frontend/book-recommend-service/src/App.js
@@ -1,10 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
-import IntroPage from './IntroPage';
+import './styles/index.css';
+import IntroPage from './pages/IntroPage';
+import HomePage from './pages/HomePage';
+import SurveyPage from './pages/SurveyPage';
 
 function App() {
   return (
     <Routes>
       <Route path='/intro' element={<IntroPage/>}/>
+      <Route path='/home' element={<HomePage/>}/>
+      <Route path='/survey' element={<SurveyPage/>}/>
     </Routes>
   );
 }
