98c79f008b6e0880f2248b1be7e22c36b3b5b94a
subin <axhtl@gachon.ac.kr>
2024-10-21 17:25:55 +0900

    chore: swagger 설정 추가, profile(local, db) 생성

--- src/main/java/com/example/bookrecommandations/SecurityConfig.java
+++ src/main/java/com/example/bookrecommandations/SecurityConfig.java
@@ -18,10 +18,10 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf
-                        .ignoringRequestMatchers("/book/signup", "/book/survey", "/api/search") // CSRF 보호에서 회원가입 경로 제외
+                        .ignoringRequestMatchers("/book/signup", "/book/survey", "/api/search", "/swagger-ui/**", "/v3/api-docs/**") // CSRF 보호에서 회원가입 경로 제외
                 )
                 .authorizeHttpRequests(authorize -> authorize // authorizeRequests() 대신 authorizeHttpRequests() 사용
-                        .requestMatchers("/book/signup", "/book/survey", "/api/search").permitAll() // 회원가입 경로 허용
+                        .requestMatchers("/book/signup", "/book/survey", "/api/search", "/swagger-ui/**", "/v3/api-docs/**").permitAll() // 회원가입 경로 허용
                         .anyRequest().authenticated() // 그 외 모든 요청은 인증 필요
                 );
 
---
+++ src/main/java/com/example/bookrecommandations/common/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.example.bookrecommandations.common;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Book Recommandations Swagger")
+                .description("도서 추천 서비스 관련 REST API")
+                .version("1.0.0");
+    }
+}
