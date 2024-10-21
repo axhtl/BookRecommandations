b7411f1cfea4273f6f33fd057716a28961f819aa
subin <axhtl@gachon.ac.kr>
2024-10-21 17:56:29 +0900

    refactor: h2 관련 링크 permitAll에 추가

--- src/main/java/com/example/bookrecommandations/SecurityConfig.java
+++ src/main/java/com/example/bookrecommandations/SecurityConfig.java
@@ -18,11 +18,16 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf
-                        .ignoringRequestMatchers("/book/signup", "/book/survey", "/api/search", "/swagger-ui/**", "/v3/api-docs/**") // CSRF 보호에서 회원가입 경로 제외
+                        .ignoringRequestMatchers("/book/signup", "/book/survey", "/api/search",
+                                "/swagger-ui/**", "/v3/api-docs/**", "/h2-console/**") // CSRF 보호에서 회원가입 경로 제외
                 )
                 .authorizeHttpRequests(authorize -> authorize // authorizeRequests() 대신 authorizeHttpRequests() 사용
-                        .requestMatchers("/book/signup", "/book/survey", "/api/search", "/swagger-ui/**", "/v3/api-docs/**").permitAll() // 회원가입 경로 허용
+                        .requestMatchers("/book/signup", "/book/survey", "/api/search", "/swagger-ui/**",
+                                "/v3/api-docs/**", "/h2-console/**").permitAll() // 회원가입 경로 허용
                         .anyRequest().authenticated() // 그 외 모든 요청은 인증 필요
+                )
+                .headers(headers -> headers
+                        .frameOptions(frameOptions -> frameOptions.sameOrigin()) // 프레임 사용을 허용
                 );
 
         return http.build();
