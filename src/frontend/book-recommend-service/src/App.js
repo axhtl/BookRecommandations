1fd739d93d9332df279e4243a70d99e8eca103ad
skyminju9 <skyminju9@naver.com>
2024-10-24 17:34:17 +0900

    feat: router 수정

--- src/frontend/book-recommend-service/src/App.js
+++ src/frontend/book-recommend-service/src/App.js
@@ -3,14 +3,28 @@ import './styles/index.css';
 import IntroPage from './pages/IntroPage';
 import HomePage from './pages/HomePage';
 import SurveyPage from './pages/SurveyPage';
+import SurveyIntroPage from './pages/SurveyIntroPage';
+import SearchPage from './pages/SearchPage';
+import PersonalPage from './pages/PersonalPage';
+import { AuthProvider } from './contexts/AuthContext';
+import { SearchProvider } from './contexts/SearchContext';
+import SignUpPage from './pages/SignUpPage';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/intro' element={<IntroPage/>}/>
-      <Route path='/home' element={<HomePage/>}/>
-      <Route path='/survey' element={<SurveyPage/>}/>
-    </Routes>
+    <AuthProvider>
+      <SearchProvider>
+        <Routes>
+        <Route path='/intro' element={<IntroPage/>}/>
+        <Route path='/home/*' element={<HomePage/>}/>
+        <Route path='/signup' element={<SignUpPage/>}/>
+        <Route path='/surveyintro' element={<SurveyIntroPage/>}/>      
+        <Route path='/survey' element={<SurveyPage/>}/>
+        <Route path='/home/search' element={<SearchPage/>}/>
+        <Route path='/home/personal' element={<PersonalPage/>}/>
+      </Routes>
+      </SearchProvider>
+    </AuthProvider>
   );
 }
 
