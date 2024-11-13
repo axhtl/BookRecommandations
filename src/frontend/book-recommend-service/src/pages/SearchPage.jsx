9483df173b166660af9bf9bfcbc772f09fa75d07
skyminju9 <skyminju9@naver.com>
2024-11-14 01:51:16 +0900

    chore: search 관련 수정중

--- src/frontend/book-recommend-service/src/pages/SearchPage.jsx
+++ src/frontend/book-recommend-service/src/pages/SearchPage.jsx
@@ -1,24 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 // import { Pc, Mobile } from "../components/reponsiveCheck";
-import { MobileNavBar, NavBar } from "../components/navBar";
+import { NavBar } from "../components/navBar";
 import { useNavigate } from "react-router-dom";
-import { Mobile, Pc } from "../components/reponsiveCheck";
-import { ReactComponent as MenuIcon } from "../assets/menu.svg";
+import { Pc } from "../components/reponsiveCheck";
+// import { ReactComponent as MenuIcon } from "../assets/menu.svg";
 // import { ReactComponent as Logo } from "../assets/logo.svg";
 
 const SearchPage = () => {
   const [keyword, setKeyword] = useState();
   const [result, setResult] = useState([]);
-  const [isClosed, setIsClosed] = useState(false);
+  // const [isClosed, setIsClosed] = useState(false);
+  // const inputRef = useRef(null);
 
   // const [selectedBook, setSelectedBook] = useState(null);
   const navigation = useNavigate();
 
-  useEffect(() => {
-    console.log(result);
-  }, [result]);
+  // useEffect(() => {
+  //   setKeyword(keyword);
+  // }, [keyword]);
 
   const searchHandler = async () => {
+    if (!keyword) {
+      alert("검색어를 입력하세요.");
+      return;
+    }
     try {
       const response = await fetch(`/api/search?query=${keyword}`);
 
@@ -102,70 +107,6 @@ const SearchPage = () => {
           </div>
         </div>
       </Pc>
-      <Mobile>
-        <div className="searchPageWrapper-mobile">
-          <div className="navWrapper-mobile">
-            <div onClick={(e) => setIsClosed(!isClosed)}>
-              <MenuIcon />
-            </div>
-            <div>
-              <p className="logoText">READING PLANNER</p>
-            </div>
-            <div style={{ width: 36, height: 36 }} />
-          </div>
-          {isClosed && (
-            <div className="mobileNav-overlay">
-              <MobileNavBar isClosed={() => setIsClosed(!isClosed)} />
-            </div>
-          )}
-          <div className="searchInput-mobile">
-            <div className="searchInputWrapper-mobile">
-              <input
-                placeholder="제목이나 저자명으로 책을 검색하세요. 🔍"
-                onChange={(e) => setKeyword(e.target.value)}
-              />
-              <button onClick={searchHandler}>search</button>
-            </div>
-          </div>
-          <div className="resultWrapper-mobile">
-            {keyword &&
-              result &&
-              result.map((item) => {
-                return (
-                  <div
-                    key={item.itemId}
-                    className="resultContainer-mobile"
-                    onClick={() => onClickBook(item)}
-                  >
-                    <img
-                      src={item.cover.replace("coversum", "cover500")}
-                      className="resultCoverImage-mobile"
-                      alt="coverimage"
-                    />
-                    <div className="resultInfos-mobile">
-                      <div className="bookInfo-mobile">
-                        <p className="bookTitle-mobile">{item.title}</p>
-                        <p className="bookAuthor-mobile">{item.author}</p>
-                        <div className="bookISBN-mobile">
-                          <p style={{ fontWeight: "500" }}>ISBN</p>
-                          <p>{item.isbn}</p>
-                        </div>
-                      </div>
-                      <div className="bookDescription-mobile">
-                        <p>{item.description}</p>
-                      </div>
-                    </div>
-                  </div>
-                );
-              })}
-            {keyword && !result && (
-              <div className="searchResultNone">
-                <p>검색 결과가 없어요. 😓</p>
-              </div>
-            )}
-          </div>
-        </div>
-      </Mobile>
     </>
   );
 };
